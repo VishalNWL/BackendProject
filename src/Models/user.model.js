48584f6086d1b413038b7fd6eb398b7bde226bcc
Vishal <nrsvishaln@gmail.com>
2025-02-11 23:07:39 +0530

    added JWT

--- src/Models/user.model.js
+++ src/Models/user.model.js
@@ -18,6 +18,10 @@ const userSchema=new mongoose.Schema({
      trim:true,
      index:true
    },
+   email:{
+    type:String,
+    required:true
+   },
    avatar:{
     type:String,//coudinary url
     required:true,
@@ -55,5 +59,33 @@ userSchema.pre("save",async function(next) {
 //here we added our own method
 userSchema.methods.isPasswordCorrect=async function(password){
    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAcessToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      username:this.username,
+      fullname:this.fullname,
+      email:this.email
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+   )
+}
+userSchema.methods.generateRefreshToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+   )
 }
 export const User=mongoose.model("User",userSchema)
